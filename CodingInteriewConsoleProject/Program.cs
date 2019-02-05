a1753b574bd79f2bc0949299378746ccf4f25565
Ashish Bhardwaj <napster.ashish@gmail.com>
2019-02-05 19:54:05 +0000

    Added solution to print a array using recursive fundtion.

---
+++ CodingInteriewConsoleProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using InterviewPractice.Others;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInteriewConsoleProject
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] arr = new int[] { 1, 3, 5, 7, 9 };
+            PrintArrayRecursive p = new PrintArrayRecursive();
+            p.PrintArray(arr);
+        }
+    }
+}
---
+++ InterviewPractice/Others/PrintArrayRecursive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPractice.Others
+{
+    public class PrintArrayRecursive
+    {
+
+        /*
+         * If an array is given 1, 2, 3, 4, 5
+            Print
+            1 2 3 4 5
+            5 4 3 2 1
+        */
+
+        public void PrintArray(int []arr, int currIndex)
+        {   
+            Console.Write(arr[currIndex]);
+            if (currIndex==arr.Length-1)
+                Console.WriteLine();                
+            else
+                PrintArray(arr, currIndex+1);
+
+            Console.Write(arr[currIndex]);
+        }
+        public void PrintArray (int[] arr)
+        {
+            PrintArray(arr, 0);
+        }
+    }
+}
