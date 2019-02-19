f04d568174576bffae2d962a41a8b8f098c6da9b
AB <napster.ashish@gmail.com>
2019-02-19 17:29:33 +0000

    Added Problem#2

---
+++ InterviewPracticeTestCases/DailyCodingProblem/Problem2Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewPractice.DailyCodingProblem;
+
+namespace InterviewPracticeTestCases.DailyCodingProblem
+{
+    [TestClass]
+    public class Problem2Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Problem2 p = new Problem2();
+            int[] arr = new int[] { 3, 2, 1 };
+            p.Solve(arr);
+            CollectionAssert.AreEqual(arr, new int[] { 2, 3, 6 });
+        }
+    }
+}
