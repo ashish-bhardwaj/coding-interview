52ca397d27a11ac9fb7d222c2641c975ec2a5473
AB <napster.ashish@gmail.com>
2019-02-19 15:16:36 +0000

    Added problem#1 from DailyCodingProblems.

---
+++ InterviewPractice/DailyCodingProblem/Problem1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPractice.DailyCodingProblem
+{
+    /* Problem Statement
+        Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+        For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+        Bonus: Can you do this in one pass?
+    */
+
+    /* Time and Space complexity
+     * Time complexity : O(n)
+     * Space Complextiy: O(n)
+     * */
+
+    /* Solution Overview
+     * Iterate through the numbers in array, get the difference of k and number in iteration in array.
+     * Store the diff in hash
+     * for each number if it exists in the diff-hashset as required for sum for another number.
+     * */
+    public class Problem1
+    {
+        public bool NumbersHaveSum (int []arr, int sum)
+        {
+            HashSet<int> hs = new HashSet<int>();
+            for (int i=0; i<arr.Length; i++)
+            {
+                if (hs.Contains(arr[i]))
+                    return true;
+                else
+                {
+                    int diff = sum - arr[i];
+                    hs.Add(diff);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ InterviewPracticeTestCases/DailyCodingProblem/Problem1Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewPractice.DailyCodingProblem;
+
+namespace InterviewPracticeTestCases.DailyCodingProblem
+{
+    [TestClass]
+    public class Problem1Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Problem1 p = new Problem1();
+            
+            bool res = p.NumbersHaveSum(new int[] { 10, 15, 3, 7 }, 17 );
+            Assert.IsTrue(res);
+        }
+    }
+}
